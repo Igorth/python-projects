55595c4b6678855acf68eee127c07206d0ba6e3d
Igor Dias <igordiasth@gmail.com>
2026-01-27 14:24:59 +0000

    feat: adding `extract_urls`

--- email-ioc/eioc.py
+++ email-ioc/eioc.py
@@ -13,8 +13,47 @@ def read_file(file_path):
     msg = parser.parsebytes(content)
     return msg
 
+def extract_ips(email_message):
+    ips = set()
+
+    # Extract IP addressess from headers
+    for header_name, header_value in email_message.items():
+        ips.update(re.findall(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', header_value))
+    
+    # Extract IP addressess from email body
+    for part in email_message.walk():
+        content_type = part.get_content_type()
+        if content_type == 'text/plain' or content_type == 'text/html':
+            payload = part.get_payload(decode=True)
+            if isinstance(payload, bytes):
+                payload = payload.decode('utf-8', errors='ignore')
+            ips.update(re.findall(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', payload))
+    
+    valid_ips = []
+    for ip in ips:
+        try:
+            ipaddress.ip_address(ip)
+            valid_ips.append(ip)
+        except ValueError:
+            pass
+    return list(set(valid_ips))
+
+def extract_urls(email_message):
+    urls = set()
+
+    for part in email_message.walk():
+        content_type = part.get_content_type()
+        if content_type == 'text/plain'or content_type == 'text/html':
+            payload = part.get_payload(decode=True)
+            if isinstance(payload, bytes):
+                payload = payload.decode('utf-8', errors='ignore')
+            urls.update(re.findall(r'https?:\/\/(?:[\w\-]+\.)+[a-z]{2,}(?:\/[\w\-\.\/?%&=]*)?', payload))
+    return list(urls)
+
 def main(file_path):
     email_message = read_file(file_path)
+    ips = extract_ips(email_message)
+    urls = extract_urls(email_message)
     print()
 
 if __name__ == "__main__":
